cc4310bc948900a5ef88587311dedbbd502b1f83
trMatherz <137810253+trMatherz@users.noreply.github.com>
2024-08-19 20:31:54 -0500

    Update P05_CountingToys_Solution.cpp

--- Alien Language/P05_CountingToys_Solution.cpp
+++ Alien Language/P05_CountingToys_Solution.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main() {
     string a, b, c; int x, y, z;
     cin >> a >> x >> b >> y >> c >> z; 
-    if(x > y && x > z) {
+    if(x > y && x > z) { //check if x is the biggest
         cout << a;
-    } else if(y > z) {
+    } else if(y > z) { //check if y is the biggest
         cout << b;
-    } else cout << c; 
+    } else cout << c;  //else c is the biggest
     return 0;
 }
