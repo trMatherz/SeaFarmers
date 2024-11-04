1ce0f7674f81bb8007598672af7db12fea2b4e9c
trMatherz <137810253+trMatherz@users.noreply.github.com>
2024-11-03 21:01:21 -0600

    Create P07_StackingPebbles_Solution.cpp

---
+++ Alien Language/P07_StackingPebbles_Solution.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n; cin >> n; 
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        int h; cin >> h; sum += h; 
+    }
+    cout << sum;
+}
