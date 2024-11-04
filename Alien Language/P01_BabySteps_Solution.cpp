02618fb31b5e4c4d6e0ba1cc6e3af19de7e35b60
trMatherz <137810253+trMatherz@users.noreply.github.com>
2024-11-03 20:57:48 -0600

    Update P01_BabySteps_Solution.cpp

--- Alien Language/P01_BabySteps_Solution.cpp
+++ Alien Language/P01_BabySteps_Solution.cpp
@@ -5,7 +5,8 @@
 #include <algorithm>
 using namespace std;
 
-//these are comments that do not affect the code
+//These are comments 
+//They do not affect the code
 int main() {
     cout << "What is up, Jerry?"; //Output the sentence
     return 0;
