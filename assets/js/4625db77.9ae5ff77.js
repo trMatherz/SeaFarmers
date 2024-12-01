05cbe7720f61f29e8e99b1832fcbd08c14d557df
jake <jakebhudman@gmail.com>
2024-12-01 16:14:39 -0600

    Updates

--- assets/js/4625db77.9ae5ff77.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9094],{3115:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/SeaFarmers/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/f8551aab.806054d9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[224],{1976:(e,t,a)=>{a.r(t),a.d(t,{default:()=>u});var l=a(6540),n=a(781);function u(){return l.createElement(n.A,null,l.createElement("h1",null,"My React page"),l.createElement("p",null,"This is a React page"))}}}]);
