07df7a886380592d01cad1575aa6b08a65f1f7c3
jake <jakebhudman@gmail.com>
2024-12-08 00:53:11 -0600

    Final today

--- src/components/SampleProblem.js
+++ src/components/SampleProblem.js
@@ -16,7 +16,7 @@ const SampleProblem = ({ moduleName, topicId, location }) => {
     async function fetchModuleData() {
       setLoading(true);
       try {
-        const userId = sessionStorage.getItem('userId');
+        const userId = sessionStorage.getItem('userId') || "guest";
 
         if (!userId) {
           throw new Error('User ID not found in session storage');
